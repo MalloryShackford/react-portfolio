f228e9ac9daa852b4201ca45e85e16415ea94119
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2023-05-11 13:39:48 -0400

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -73,18 +73,22 @@ const Projects = () => {
           viewport={{ once: true, amount: 0.2 }}
         >
           {/* ROW 1 */}
-          <div
-            className="flex justify-center text-center items-center p-10 bg-red
-              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
-          >
-            BEAUTIFUL USER INTERFACES
-          </div>
+//           <div
+//             className="flex justify-center text-center items-center p-10 bg-red
+//               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+//           >
+//             BEAUTIFUL USER INTERFACES
+//           </div>
           
           
-            <a href="https://lyriks-sigma.vercel.app/">
-            <img src="https://i.postimg.cc/PrfwJ9VJ/lyriks.png" /></a>
+//             <a href="https://lyriks-sigma.vercel.app/">
+//             <img src="https://i.postimg.cc/PrfwJ9VJ/lyriks.png" /></a>
 
-          <a href="https://client-psi-wheat.vercel.app/">
+          
+           <a href="https://cheftimmaslow.com">
+            <img src="https://i.ibb.co/FBt09C6/utr.png" /></a>
+            
+            <a href="https://client-psi-wheat.vercel.app/">
           <img src="https://i.ibb.co/9pH4Qw4/tinder.png" /></a>
           
           <a href="https://brilliant-sopapillas-6fd86d.netlify.app/">
@@ -99,18 +103,18 @@ const Projects = () => {
           <a href="https://rainbow-praline-272e56.netlify.app/">
             <img src="https://i.ibb.co/V9C4CJn/fb.png" /></a>
           
-          <a href="https://underr.vercel.app/">
+          <a href="https://cheftimmaslow.com">
             <img src="https://i.ibb.co/FBt09C6/utr.png" /></a>
           
           
           
 
-          <div
-            className="flex justify-center text-center items-center p-10 bg-blue
-              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
-          >
-            SMOOTH USER EXPERIENCE
-          </div>
+//           <div
+//             className="flex justify-center text-center items-center p-10 bg-blue
+//               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+//           >
+//             SMOOTH USER EXPERIENCE
+//           </div>
         </motion.div>
       </div>
     </section>
